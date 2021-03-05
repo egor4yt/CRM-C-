0d040d3e96e79681dd6169e1311f636ade14f71d
Egor <egor4yt@gmail.com>
2021-03-05 14:34:21 +0700

    Изменения Design - разработана страница настроек приложения - Названия стобцов с заметками у клиентов теперь загружается из сохранённых настроек
    =========
    
    Изменения Backend
    - разработана страница настроек приложения
    - окно приложения теперь можно переносить
    - приложение теперь запускается в центре экрана
    - в PageLoaded для страницы с клиентами два столбца заметок получают заголовки из настроек
    
    =========
    
    Изменения SDK
    - в CSV добавлена функция SaveApplicationData для сохранения даннх приложения
    - в CSV добавлена функция LoadApplicationData для загрузки даннх приложения
    - в Domain добавлен класс настроке приложения
    - добавлены тесты для проверки сохранения и загрузки настроек приложения
    - все файлы для сохранения теперь имеют расширение .fcd

---
+++ Domain/ApplicationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace EErmakov.SoftwareDevelop.Domain
+{
+    public static class ApplicationSettings
+    {
+        public static string CompanyName = "FreeCRM";
+        public static string FirstNoteClient = "Заметка 1";
+        public static string SecondNoteClient = "Заметка 2";
+    }
+}
